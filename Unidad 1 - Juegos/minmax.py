617292a03e4a130705e9801ec4a29fa34bb2d142
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-13 19:24:58 -0300

    add: formatted code with autopep8

--- Unidad 1 - Juegos/minmax.py
+++ Unidad 1 - Juegos/minmax.py
@@ -1,15 +1,14 @@
 matrix = [[-3, -2, 6], [2, 0, 2], [5, -2, -4]]
 
-minimo = list(map(min,matrix))
+minimo = list(map(min, matrix))
 maximin = max(minimo)
 print('Maximin: ', maximin)
 
 transpose = list(zip(*matrix))
-maximo = list(map(max,transpose))
+maximo = list(map(max, transpose))
 minimax = min(maximo)
-print('Maximin: ',minimax)
+print('Maximin: ', minimax)
 
-#Extremo
-print('Maximin', max(list(map(min,matrix))))
-print('Minimax',min(list(map(max, list(zip(*matrix))))))
-
+# Extremo
+print('Maximin', max(list(map(min, matrix))))
+print('Minimax', min(list(map(max, list(zip(*matrix))))))
